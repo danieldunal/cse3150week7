1ae4eba9f90c37ba96cab72af7ae6ccee7585253
danieldunal <144380099+danieldunal@users.noreply.github.com>
2024-03-09 00:13:08 -0500

    Add files via upload

--- lambda1.cpp
+++ lambda1.cpp
@@ -3,12 +3,9 @@
 #include <algorithm>
 #include <string>
 
-using namespace std;
+#include "perfect_num.h"
 
-void display(const vector<int> & vect) {      //Prints all elements in a vector
-  for_each(vect.begin(), vect.end(), [](int elt){ cout << elt << " "; });
-  cout << endl;
-}
+using namespace std;
 
 int main() {
 
@@ -20,24 +17,7 @@ int main() {
 
     given_int = stoi(line);
 
-    if (given_int <= 0) {
-        cout << "false" << endl;
-        return 0;
-    }
-
-    vector<int> rising(given_int-1);
-    vector<int> divisors;
-
-    int indx = 1;
-    int sum = 0;
-    bool perfect = false;
-    
-    for_each(rising.begin(), rising.end(), [&indx](int & x) { x = indx++; });
-    for_each(rising.begin(), rising.end(), [&indx, &divisors, &given_int](int & x) {if(given_int%x == 0){ divisors.push_back(x); }});
-    for_each(divisors.begin(), divisors.end(), [&sum](int & x) mutable {sum += x;});
-
-    (sum == given_int) ? perfect = true : perfect = false;
-    string output = (perfect) ? "true" : "false";  
+    bool output = perfect_num(given_int);
 
     cout << output << endl;
 
